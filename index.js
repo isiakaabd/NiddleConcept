4a3d7e333717be638bb89170869809c52c30610f
Isiaka Abdulahi <isiakaabd@gmail.com>
2021-06-25 22:38:21 +0100

    second commit

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 const buttonToggler = document.querySelector('.navbar-toggler')
 const buttonTogglers = document.querySelector('.navbar-toggler-icon');
-
+const navbar= document.getElementById("navbar")
 
 buttonToggler.addEventListener('click', () => {
   buttonTogglers.classList.toggle('none')
   buttonTogglers.nextElementSibling.classList.toggle('none')
 })
 
+window.addEventListener("scroll", ()=>{
+  
+  navbar.classList.toggle("sticky",window.scrollY  > 0)
+   })
 
 
 // jquery
